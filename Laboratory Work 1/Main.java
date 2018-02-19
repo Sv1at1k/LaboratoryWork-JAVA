7452f660c385466f21e2c6629b294132b9138405
Sviatoslav Yatsynyk <sviatik1208@gmail.com>
2018-02-19 08:00:24 +0200

    Variable starts from lower case

--- Laboratory Work 1/Main.java
+++ Laboratory Work 1/Main.java
@@ -2,29 +2,29 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Flower Chamomile = new Flower();
-		Flower Tulips= new Flower("tulips",20,10,15);
-		Flower Roses = new Flower("roses",10,3,"Red",0);
-		Chamomile.setName("chamomiles");
-		Chamomile.setAmount(100);
-		Chamomile.setPrice(2);
-		System.out.println(Chamomile.toString());
-		System.out.println(Tulips.toString());
-		System.out.println(Roses.toString());
+		Flower ñhamomile = new Flower();
+		Flower tulips= new Flower("tulips",20,10,15);
+		Flower roses = new Flower("roses",10,3,"Red",0);
+		chamomile.setName("chamomiles");
+		chamomile.setAmount(100);
+		chamomile.setPrice(2);
+		System.out.println(chamomile.toString());
+		System.out.println(tulips.toString());
+		System.out.println(roses.toString());
 		Flower.printStaticSum();
-		Chamomile.printSum();
-		Tulips.printSum();
-		Roses.printSum();
-		Tulips.resetValue("tulips", 40, 15,"black" ,0);
-		Roses.resetValue("roses", 1, 100500,"white",0);
-		Chamomile.resetValue("chamomile", 9, 0.5, "white", 90);
-		System.out.println(Chamomile.toString());
-		System.out.println(Tulips.toString());
-		System.out.println(Roses.toString());
+		chamomile.printSum();
+		tulips.printSum();
+		toses.printSum();
+		tulips.resetValue("tulips", 40, 15,"black" ,0);
+		roses.resetValue("roses", 1, 100500,"white",0);
+		chamomile.resetValue("chamomile", 9, 0.5, "white", 90);
+		System.out.println(chamomile.toString());
+		System.out.println(tulips.toString());
+		System.out.println(roses.toString());
 		Flower.printStaticSum();
-		Chamomile.printSum();
-		Tulips.printSum();
-		Roses.printSum();
+		chamomile.printSum();
+		tulips.printSum();
+		roses.printSum();
 	}
 
 }
